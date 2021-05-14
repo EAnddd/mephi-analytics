ca708f7d4b08579cd35069691524e859c3ebbd88
MsLi <lizaandronova96@gmail.com>
2021-05-14 19:35:17 +0300

    add reame

--- src/main/java/org/example/streaming/analytics/controllers/StartController.java
+++ src/main/java/org/example/streaming/analytics/controllers/StartController.java
@@ -5,12 +5,10 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.RequestAttribute;
 import org.example.streaming.analytics.clients.FutureClient;
-import org.example.streaming.analytics.clients.dto.MainWeatherDto;
 import org.example.streaming.analytics.messaging.ResultProducerImpl;
 import org.example.streaming.analytics.services.ArggregationService;
 
 import javax.inject.Inject;
-import java.util.List;
 
 @Controller
 public class StartController {
@@ -29,10 +27,6 @@ public class StartController {
     @Consumes("application/x-www-form-urlencoded")
     public void send(@RequestAttribute("token") String token){
         futureClient.createUris(token);
-//        List<MainWeatherDto> a = futureClient.getStatistics();
-//        resultProducer.init();
-//        MainWeatherDto b = arggregationService.aggregate(a).get();
-//        resultProducer.produceResult(b);
     }
 
 }
